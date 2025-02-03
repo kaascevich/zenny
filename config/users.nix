61e55ae3ca0ae4d7c07f14c34d05226df3944167
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2025-02-03 17:32:28 -0500

    remove some redundant options

--- config/users.nix
+++ config/users.nix
@@ -2,9 +2,8 @@
   users = {
     mutableUsers = false;
     users.kaleb = {
+      description = "Kaleb A. Ascevich";
       isNormalUser = true;
-      home = "/home/kaleb";
-      createHome = true;
 
       extraGroups = [ "wheel" ];
       initialHashedPassword = "$y$j9T$joMu5gyUwnGVet8lwCckB0$o/k/B1BR8NiQ0xU6NgfBKxn5pnUpTmcaseUjGH2LTA5";
