74af72451f80824640384513d069c7d08962bb9c
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2025-02-03 21:19:31 -0500

    add explicit home dir creation

--- config/users.nix
+++ config/users.nix
@@ -5,6 +5,9 @@
       description = "Kaleb A. Ascevich";
       isNormalUser = true;
 
+      home = "/home/kaleb";
+      createHome = true;
+
       extraGroups = [ "wheel" ];
       initialHashedPassword = "$y$j9T$joMu5gyUwnGVet8lwCckB0$o/k/B1BR8NiQ0xU6NgfBKxn5pnUpTmcaseUjGH2LTA5";
 
