b0b079d43a5006447a285a22c61ff6dad047db92
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2025-02-05 12:31:51 -0500

    remove quotes around URLs

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "zenny config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = github:nixos/nixpkgs?ref=nixos-unstable;
     disko = {
-      url = "github:nix-community/disko";
+      url = github:nix-community/disko;
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence = {
-      url = "github:nix-community/impermanence";
+      url = github:nix-community/impermanence;
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
     #lanzaboote = {
