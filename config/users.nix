d1972d448601ba06ef8e12dde96e25f059f11788
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2025-02-05 09:43:16 -0500

    add my user to `networkmanager` group

--- config/users.nix
+++ config/users.nix
@@ -5,7 +5,7 @@
       description = "Kaleb A. Ascevich";
       isNormalUser = true;
 
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "networkmanager" ];
       initialHashedPassword = "$y$j9T$joMu5gyUwnGVet8lwCckB0$o/k/B1BR8NiQ0xU6NgfBKxn5pnUpTmcaseUjGH2LTA5";
 
       shell = pkgs.nushell;
