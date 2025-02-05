e33664b62116c5a39cbe34e5d28cd362e0d90ccf
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2025-02-05 17:39:39 -0500

    try extra-experimental-features again

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ diskName="$1"
 
 mount -o remount,size=12G,noatime /nix/.rw-store
 
-nix --experimental-features "nix-command flakes pipe-operators" \
+nix --experimental-features "nix-command flakes" \
   run "github:nix-community/disko/latest#disko-install" -- \
+  --option extra-experimental-features "pipe-operators" \
   --flake .#zenny --disk main "$diskName"
