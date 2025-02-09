d3a5b4e59ed157a8ab8e91efd6d18934ca2fb627
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2025-02-09 09:46:12 -0500

    add disk name sanity check to install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,20 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 if [[ $# -ne 1 ]]; then
-  echo "must provide one argument (device path to disk to install on)"
+  echo "must provide one argument (/dev name of disk to install on)"
   exit 1
 fi
-diskName="$1"
+disk="/dev/$1"
+
+if [[ ! -e "$disk" ]]; then
+  echo "no disk named $disk"
+  exit 1
+fi
+
 
 mount -o remount,size=12G,noatime /nix/.rw-store
 
 nix --experimental-features "nix-command flakes" \
   run "github:nix-community/disko/latest#disko-install" -- \
   --option extra-experimental-features "pipe-operators" \
-  --flake .#zenny --disk main "$diskName"
+  --flake .#zenny --disk main "$disk"
